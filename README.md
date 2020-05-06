acd21e93dacc9602f273dfedc7c740f4c4d7923b
ahmedvuqarsoy <ahmedvuqarsoy@gmail.com>
2020-05-07 00:21:43 +0400

    Pizza Project Final🍕

---
+++ README.md
@@ -0,0 +1,144 @@
+# Pizza Ordering System
+
+[![forthebadge made-with-python](http://ForTheBadge.com/images/badges/made-with-python.svg)](https://www.python.org/)
+[![ForTheBadge built-by-developers](http://ForTheBadge.com/images/badges/built-by-developers.svg)](https://GitHub.com/ahmedvuqarsoy/)
+[![ForTheBadge built-with-love](http://ForTheBadge.com/images/badges/built-with-love.svg)](https://GitHub.com/ahmedvuqarsoy/)
+[![ForTheBadge uses-git](http://ForTheBadge.com/images/badges/uses-git.svg)](https://GitHub.com/)
+[![ForTheBadge uses-html](http://ForTheBadge.com/images/badges/uses-html.svg)](https://GitHub.com/)
+[![ForTheBadge uses-css](http://ForTheBadge.com/images/badges/uses-css.svg)](https://GitHub.com/)
+
+## Properties
+
+✅ User Registration
+
+✅ User Login/Logout
+
+✅ Adding Items to Card
+
+✅ Submit Orders
+
+✅ Payment with Stripe API
+
+✅ CRUD applications to menu by Superuser
+
+✅ Not only Pizza, but also Salads, Pasta etc.
+
+✅ Add different toppings to Pizza
+
+✅ Different sized Pizzas (Small-Large)
+
+✅ View Orders
+
+✅ Responsive Website
+
+✅ Minimalistic Design
+
+## Can be done additionally
+
+▶ Upgrade Design(more interactivity, carousel, landing page, stock images)
+
+▶ Contact Page
+
+▶ Address Line
+
+▶ Changing password
+
+▶ Sending email for password changing
+
+▶ Make wishlist of user
+
+▶ Add image part for books
+
+## Set up project
+
+```
+virtualenv env
+cd .\env\Scripts\activate
+pip install -r requirements.txt
+SET FLASK_APP=application.py
+SET FLASK_DEBUG=1
+SET DATABASE_URL=[here the database link]
+```
+
+## Requirements
+
+```
+certifi==2019.6.16
+chardet==3.0.4
+defusedxml==0.6.0
+Django==2.2.10
+django-allauth==0.39.1
+django-crispy-forms==1.7.2
+idna==2.8
+oauthlib==3.1.0
+Pillow==6.2.2
+python3-openid==3.1.0
+pytz==2019.1
+requests==2.22.0
+requests-oauthlib==1.2.0
+sqlparse==0.3.0
+stripe==1.82.1
+urllib3==1.25.3
+```
+
+## Snippets
+
+### Index page
+
+<p align="center">
+  <img src="ss/index.png">
+</p>
+
+### Register page
+
+<p align="center">
+  <img src="ss/register.png">
+</p>
+
+### Login page
+
+<p align="center">
+  <img src="ss/login.png">
+</p>
+
+### Logout page
+
+<p align="center">
+  <img src="ss/logout.png">
+</p>
+
+### Profile page
+
+<p align="center">
+  <img src="ss/profile.png">
+</p>
+
+### Create page
+
+<p align="center">
+  <img src="ss/create.png">
+</p>
+
+### Cart page
+
+<p align="center">
+  <img src="ss/order-summary.png">
+</p>
+
+### Checkout page
+
+<p align="center">
+  <img src="ss/checkout.png">
+</p>
+
+### History page
+
+<p align="center">
+  <img src="ss/order-history.png">
+</p>
+
+### Admin page
+
+<p align="center">
+  <img src="ss/admin.png">
+</p>
---
+++ orders/forms.py
@@ -0,0 +1,66 @@
+from django import forms
+from .models import Item, Meal, Meal_Type, Size, Meal_Addition, Price
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ('meal', 'meal_type', 'size', 'meal_addition')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # set dropdown menus as empty until a meal is selected from the first dropdown menu
+        self.fields['meal_type'].queryset = Meal_Type.objects.none()
+        self.fields['size'].queryset = Size.objects.none()
+        self.fields['meal_addition'].queryset = Meal_Addition.objects.none()
+
+        # if a meal has been selected from the dropdown menu....
+        if 'meal' in self.data:
+            try:
+                meal_id = int(self.data.get('meal'))
+                self.fields['meal_type'].queryset = Meal_Type.objects.filter(meal_id=meal_id).order_by('meal')
+            except (ValueError, TypeError):
+                pass  # invalid input from the client; ignore and fallback to empty queryset
+        
+        # if a meal type has been selected from the dropdown menu....
+        if 'meal_type' in self.data:
+            try:
+                meal_type = int(self.data.get('meal_type'))
+                self.fields['size'].queryset = Size.objects.filter(meal_type=meal_type).order_by('size')
+                self.fields['meal_addition'].queryset = Meal_Addition.objects.filter(meal_type=meal_type).order_by('name')
+            except (ValueError, TypeError):
+                pass  # invalid input from the client; ignore and fallback to empty queryset
+        elif self.instance.pk:
+            self.fields['size'].queryset = self.instance.meal_type.size_set.order_by('size')
+            self.fields['meal_addition'].queryset = self.instance.meal_type.meal_addition_set.order_by('name')
+
+    # validate the number of toppings and get the total price of the order
+    def clean_meal_addition(self):
+        meal_addition = self.cleaned_data['meal_addition']
+        meal_type = self.cleaned_data['meal_type']
+        size = self.cleaned_data['size']
+        meal = str(self.cleaned_data['meal'])
+
+        # if a pizza is selected then validate the number of toppings
+        if 'Pizza' in meal:
+            if '1 topping' in str(meal_type):
+                if len(meal_addition) != 1:
+                    raise forms.ValidationError('You must select 1 topping')
+            if '2 toppings' in str(meal_type):
+                if len(meal_addition) != 2:
+                    raise forms.ValidationError('You must select 2 toppings')
+            if '3 toppings' in str(meal_type):
+                if len(meal_addition) != 3:
+                    raise forms.ValidationError('You must select 3 toppings')
+
+        # get the price for the meal type selected
+        meal_price = Price.objects.filter(meal_type=meal_type, size=size)
+        total_price = float()
+        for item in meal_price:
+            total_price = float(item.price)
+        
+        # if the meal type is a sub then add the cost of any additions to the total price
+        if 'Sub' in meal:
+            for item in meal_addition:
+                total_price = total_price + float(item.price)
+                
+        return meal_addition
